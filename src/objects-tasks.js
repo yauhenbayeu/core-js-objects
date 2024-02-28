ed3f18ce82d87bd4fdf40ef5dc1542c7fe004d8f
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-02-29 00:42:05 +0100

    feat: task getJSON complete

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -253,9 +253,11 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
+// getJSON([1, 2, 3]);
+// getJSON({ width: 10, height: 20 });
 
 /**
  * Returns the object of specified type from JSON representation
